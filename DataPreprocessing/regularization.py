d89d2b467aab0e50631c8fdfecfc1ee1be2cae89
Yu Yue <yyu196@Yus-MacBook-Pro.local>
2017-07-26 12:24:38 -0400

    Read Wine Data

---
+++ DataPreprocessing/regularization.py
@@ -0,0 +1,6 @@
+# Implement Logistic Regression using L1 Regularization
+import sys
+sys.path.append('../Utilities/')
+import readData as RD
+
+DataMap = RD.readDataFromWine()
--- Utilities/readData.py
+++ Utilities/readData.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
 def readDataFromIris():
 	# Use pandas library to import all data into a framework and 
@@ -20,3 +23,38 @@ def readDataFromIris():
 	X = df.iloc[0: 100, [0, 2]].values
 
 	return {'X': X, 'y': y}
+
+def readDataFromWine():
+	df_wine = pd.read_csv('../Utilities/wine.data', header = None)
+	df_wine.columns = ['Class label', 'Alcohol',
+					   'Malic acid', 'Ash',
+					   'Alcalinity of ash', 'Magnesium',
+					   'Total phenols', 'Flavanoids',
+					   'Nonflavanoid phenols',
+					   'Proanthocyanins',
+					   'Color intensity', 'Hue',
+					   'OD280/OD315 of diluted wines',
+					   'Proline']
+	print('Class labels have values ', np.unique(df_wine['Class label']))
+	print('Print first five rows of the dataset')
+	print(df_wine.head())
+
+	# Split data
+	X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+	X_train, X_test, y_train, y_test = \
+		train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+	# Normalize the data
+	mms = MinMaxScaler()
+	X_train_norm = mms.fit_transform(X_train)
+	X_test_norm = mms.transform(X_test)
+
+	# Standardize the data
+	stdsc = StandardScaler()
+	X_train_std = stdsc.fit_transform(X_train),
+	X_test_std = stdsc.transform(X_test)
+
+	return {'X_train': X_train, 'y_train': y_train,
+			'X_test': X_test, 'y_test':y_test,
+			'X_train_norm': X_train_norm, 'X_test_norm': X_test_norm,
+			'X_train_std': X_train_std, 'X_test_std': X_test_std}
